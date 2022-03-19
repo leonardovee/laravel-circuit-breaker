c697f78e6ef46ab647bd02438894b56596b47271
leonardovee <leoveeph@gmail.com>
2022-03-18 22:04:25 -0300

    feat: add config file

--- config/circuit-breaker.php
+++ config/circuit-breaker.php
@@ -1,8 +1,15 @@
 <?php
 
 /**
- * Config for \LeonardoVee\CircuitBreaker
+ * Config for \LeonardoVee\CircuitBreaker\CircuitBreaker
  */
 return [
-
+    'memcached-server' => [
+        'host' => '127.0.0.1',
+        'port' => 11211
+    ],
+    'circuit-breaker' => [
+        'failure-threshold' => 5,
+        'timeout' => 5
+    ]
 ];

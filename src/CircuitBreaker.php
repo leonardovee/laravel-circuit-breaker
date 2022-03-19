e9ef999b061a77d66448976e641a0ce9a6cac304
leonardovee <leoveeph@gmail.com>
2022-03-18 22:04:46 -0300

    feat: implement circuit breaker

--- src/CircuitBreaker.php
+++ src/CircuitBreaker.php
@@ -2,6 +2,101 @@
 
 namespace LeonardoVee\CircuitBreaker;
 
+use Memcached;
+
 class CircuitBreaker
 {
+    public static string $memcachedServerHost = 'localhost';
+    public static int $memcachedServerPort = 11211;
+    private static string $memcachedServerPrefix = 'circuit-breaker-';
+
+    private const memcachedServerCircuitFailuresCountSuffix = '-failures-count';
+    private const memcachedServerCircuitOpenSuffix = '-is-open';
+
+    public static function isAvailable(string $circuitName): bool
+    {
+        if (self::isCircuitOpen($circuitName)) {
+            return false;
+        }
+
+        if (self::isCircuitThresholdReached($circuitName)) {
+            self::openCircuit($circuitName);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function setSuccess(string $circuitName): void
+    {
+        $memcachedServer = self::getMemcachedServer();
+
+        $memcachedServer->delete(
+            key: self::$memcachedServerPrefix . $circuitName . self::memcachedServerCircuitFailuresCountSuffix
+        );
+    }
+
+    public static function setFailure(string $circuitName)
+    {
+        $memcachedServer = self::getMemcachedServer();
+
+        $doMemcachedServerHasFailuresCountKey = $memcachedServer->get(
+            key: self::$memcachedServerPrefix . $circuitName . self::memcachedServerCircuitFailuresCountSuffix
+        );
+
+        if (! $doMemcachedServerHasFailuresCountKey) {
+            $memcachedServer->set(
+                key: self::$memcachedServerPrefix . $circuitName . self::memcachedServerCircuitFailuresCountSuffix,
+                value: 0,
+                expiration: config(key: 'circuit-breaker.circuit-breaker.timeout')
+            );
+        }
+
+        $memcachedServer->increment(
+            key: self::$memcachedServerPrefix . $circuitName . self::memcachedServerCircuitFailuresCountSuffix
+        );
+    }
+
+    private static function getMemcachedServer(): Memcached
+    {
+        $memcachedServer = new Memcached();
+
+        $memcachedServer->addServer(
+            host: config(key: 'circuit-breaker.memcached-server.host') ?? self::$memcachedServerHost,
+            port: config(key: 'circuit-breaker.memcached-server.port') ?? self::$memcachedServerPort
+        );
+
+        return $memcachedServer;
+    }
+
+    private static function isCircuitOpen(string $circuitName): bool
+    {
+        $memcachedServer = self::getMemcachedServer();
+
+        return $memcachedServer->get(
+            key: self::$memcachedServerPrefix . $circuitName . self::memcachedServerCircuitOpenSuffix
+        );
+    }
+
+    private static function isCircuitThresholdReached(string $circuitName): bool
+    {
+        $memcachedServer = self::getMemcachedServer();
+
+        $failuresCount = $memcachedServer->get(
+            key: self::$memcachedServerPrefix . $circuitName . self::memcachedServerCircuitFailuresCountSuffix
+        );
+
+        return $failuresCount > config(key: 'circuit-breaker.circuit-breaker.failure-threshold');
+    }
+
+    private static function openCircuit(string $circuitName): void
+    {
+        $memcachedServer = self::getMemcachedServer();
+
+        $memcachedServer->set(
+            key: self::$memcachedServerPrefix . $circuitName . self::memcachedServerCircuitOpenSuffix,
+            value: true,
+            expiration: config(key: 'circuit-breaker.circuit-breaker.timeout')
+        );
+    }
 }
--- tests/CircuitBreakerTest.php
+++ tests/CircuitBreakerTest.php
@@ -1,5 +1,54 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use LeonardoVee\CircuitBreaker\CircuitBreaker;
+
+beforeEach(function () {
+    $memcachedServer = new Memcached();
+    $memcachedServer->addServer(
+        host: CircuitBreaker::$memcachedServerHost,
+        port: CircuitBreaker::$memcachedServerPort
+    );
+    $memcachedServer->flush();
+});
+
+it(description: 'should return if the circuit is available', closure: function () {
+    $isCircuitAvailable = CircuitBreaker::isAvailable(circuitName: 'testing');
+    expect(value: $isCircuitAvailable)->toBeTrue();
+});
+
+it(description: 'should open the circuit on failure', closure: function () {
+    for ($i = 0; $i < 6; $i++) {
+        CircuitBreaker::setFailure(circuitName: 'testing');
+    }
+
+    /** force a CircuitBreaker::isCircuitOpen() call on next CircuitBreaker::isAvailable() */
+    CircuitBreaker::isAvailable(circuitName: 'testing');
+
+    $isCircuitAvailable = CircuitBreaker::isAvailable(circuitName: 'testing');
+    expect(value: $isCircuitAvailable)->toBeFalse();
+});
+
+it(description: 'should closed the circuit after a success', closure: function () {
+    for ($i = 0; $i < 6; $i++) {
+        CircuitBreaker::setFailure(circuitName: 'testing');
+    }
+
+    CircuitBreaker::setSuccess(circuitName: 'testing');
+
+    $isCircuitAvailable = CircuitBreaker::isAvailable(circuitName: 'testing');
+    expect(value: $isCircuitAvailable)->toBeTrue();
+});
+
+it(description: 'should close the circuit after 15 seconds', closure: function () {
+    for ($i = 0; $i < 6; $i++) {
+        CircuitBreaker::setFailure(circuitName: 'testing');
+    }
+
+    /** force a CircuitBreaker::isCircuitOpen() call on next CircuitBreaker::isAvailable() */
+    CircuitBreaker::isAvailable(circuitName: 'testing');
+
+    sleep(seconds: config(key: 'circuit-breaker.circuit-breaker.timeout'));
+
+    $isCircuitAvailable = CircuitBreaker::isAvailable(circuitName: 'testing');
+    expect(value: $isCircuitAvailable)->toBeTrue();
 });
